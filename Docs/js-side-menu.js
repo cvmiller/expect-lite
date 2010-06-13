c29d2f49c71b08cbeab586099a3052375024692c
Craig Miller <cvmiller@sawtoothy.(none)>
2010-06-13 10:32:34 -0400

    Preparation for Documentation release 3.6.1 No change to expect-lite, but documentation has been updated

---
+++ Docs/js-side-menu.js
@@ -0,0 +1,17 @@
+// external js code
+	
+// print array of lines, which becomes the side menu
+
+menu_lines = [ '<ul>' , '<li><a href="expect-lite_simple.html">What is it?</a></li>', '<li><a href="expect-lite_install.html">Downloading and Installing</a></li>' , '<li><a href="expect-lite_doc.html">Documentation</a></li>' , '<li><a href="expect-lite_tips.html">Debugging Tips and Tricks</a></li>' ,'<li><a href="expect-lite_license.html">BSD-Style License</a></li>' ,'</ul>' ,''  ];
+
+
+function print_menu_lines (lines) {
+	var line_break = '<br/>';
+	for (var line in lines) {
+		document.writeln(lines[line]);
+	}	
+}
+
+print_menu_lines(menu_lines);
+
+
