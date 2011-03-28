32aea03f013586cf844d39c16969f534e20035e8
Craig Miller <cvmiller@sawtoothy.(none)>
2011-03-27 20:28:08 -0400

    Updated sudo example comments

--- sudo.tcl
+++ sudo.tcl
@@ -1,5 +1,12 @@
+# Native Expect function to handle sudo prompt
+# sudo is sometimes challenged by a password, and sometimes not
+#
+# by Craig Miller	June 2010
+#
+
 proc run_sudo { cmd pass } {
-	global DEBUG expect_out
+	global expect_out
+	upvar expectlite::DEBUG DEBUG
 	send "sudo -p 'sudo# ' $cmd \n"
 	expect  -timeout 1 -notransfer -re "\nsudo# " {  send "$pass\n"; puts "sendpass\n"}
 	# wait for bash prompt	
