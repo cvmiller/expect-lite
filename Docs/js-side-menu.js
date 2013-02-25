d271030b14a46997d2ad34c88692f49e2d22f87b
Craig Miller <cvmiller@gmail.com>
2013-02-24 19:50:26 -0500

    14 February 2013 - 4.4.2d7 	Fixed color bug using *DEBUG
    24 February 2013 -
    	Updated docs with new features: else & EL_INFINITE_LOOP env var

--- Docs/js-side-menu.js
+++ Docs/js-side-menu.js
@@ -2,7 +2,7 @@
 	
 // print array of lines, which becomes the side menu
 
-menu_lines = [ '<ul>' , '<li><a href="expect-lite_simple.html">What is it?</a></li>', '<li><a href="expect-lite_install.html">Downloading and Installing</a></li>' , '<li><a href="expect-lite_doc.html">Documentation</a></li>' , '<li><a href="expect-lite_tips.html">Debugging Tips and Tricks</a></li>' ,'<li><a href="expect-lite_license.html">BSD-Style License</a></li>' ,'<li><a href="expect-lite_cygwin.html">Cygwin Support</a></li>','<li><a href="expect-lite_library.html">TCL Library Support</a></li>','<li><a href="expect-lite_press.html">News & Reviews</a></li>','<li><a href="http://expect-lite.blogspot.ca">Blog</a></li>','</ul>' ,''  ];
+menu_lines = [ '<ul>' , '<li><a href="expect-lite_simple.html">What is it?</a></li>', '<li><a href="expect-lite_install.html">Downloading and Installing</a></li>' , '<li><a href="expect-lite_doc.html">Documentation</a></li>' , '<li><a href="expect-lite_tips.html">Debugging Tips and Tricks</a></li>' ,'<li><a href="expect-lite_license.html">BSD-Style License</a></li>' ,'<li><a href="expect-lite_cygwin.html">Cygwin Support</a></li>','<li><a href="expect-lite_library.html">TCL Library Support</a></li>','<li><a href="expect-lite_press.html">News & Reviews</a></li>','<li><a href="http://expect-lite.blogspot.ca">Blog - with cool tips</a></li>','<li><a href="http://sourceforge.net/projects/expect-lite/files/"><b><big>Download </big></b></a></li>','</ul>' ,''  ];
 
 
 function print_menu_lines (lines) {
